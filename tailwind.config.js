e5ddbd201eb75e1d2a90df97fe5f6fced7fa7f4c
one-liang <william810212@gmail.com>
2024-07-09 01:15:38 +0800

    feat: add section 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
   content: ["./**/*.{html,js}"],
-  prefix: 'iwh-',
+  prefix: "iwh-",
   theme: {
     extend: {
       colors: {
@@ -10,36 +8,66 @@ module.exports = {
           800: "#37373D",
           500: "#838383",
           100: "#EEEEEE",
-        }
+        },
       },
       screens: {
-        "xl": "1200px",
+        xl: "1200px",
       },
     },
     fontFamily: {
-      'sans': ['-apple-system', 'BlinkMacSystemFont', 'Arial', 'Segoe UI', 'Roboto', 'Noto Sans TC', 'PingFang TC', 'Microsoft JhengHei', 'sans-serif'],
+      sans: [
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Arial",
+        "Segoe UI",
+        "Roboto",
+        "Noto Sans TC",
+        "PingFang TC",
+        "Microsoft JhengHei",
+        "sans-serif",
+      ],
     },
     fontSize: {
-      'base': ['1rem', {
-        lineHeight: '1.5rem',
-      }],
-      'lg': ['1.125rem', {
-        lineHeight: '1.75rem',
-      }],
-      'xl': ['1.25rem', {
-        lineHeight: '1.75rem',
-      }],
-      '2xl': ['1.5rem', {
-        lineHeight: '2rem',
-      }],
-      '3xl': ['1.875rem', {
-        lineHeight: '2.25rem',
-      }],
-      '4xl': ['2.25rem', {
-        lineHeight: '2.813rem',
-      }],
-    }
+      base: [
+        "1rem",
+        {
+          lineHeight: "1.5rem",
+        },
+      ],
+      lg: [
+        "1.125rem",
+        {
+          lineHeight: "1.75rem",
+        },
+      ],
+      xl: [
+        "1.25rem",
+        {
+          lineHeight: "1.75rem",
+        },
+      ],
+      "2xl": [
+        "1.5rem",
+        {
+          lineHeight: "2rem",
+        },
+      ],
+      "3xl": [
+        "1.875rem",
+        {
+          lineHeight: "2.25rem",
+        },
+      ],
+      "4xl": [
+        "2.25rem",
+        {
+          lineHeight: "2.813rem",
+        },
+      ],
+    },
+    transitionTimingFunction: {
+      zoom: "cubic-bezier(0.18, 0.6, 0.33, 0.76)",
+    },
   },
   plugins: [],
-}
-
+};
