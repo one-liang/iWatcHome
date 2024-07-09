1b6f74df6019f54a7f0dea9d2b124ee56379c13d
one-liang <william810212@gmail.com>
2024-07-10 00:11:45 +0800

    feat: add section 4

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-console.log("index.js");
+!function(){var e=document.getElementById("video2"),d=document.getElementById("playVideoBtn"),i=document.getElementById("pauseVideoBtn");d.addEventListener("click",(function(){e.play(),d.classList.add("iwh-hidden"),i.classList.remove("iwh-hidden")})),i.addEventListener("click",(function(){e.pause(),d.classList.remove("iwh-hidden"),i.classList.add("iwh-hidden")}))}();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('index.js')
+const video2 = document.getElementById("video2");
+const playVideoBtn = document.getElementById("playVideoBtn");
+const pauseVideoBtn = document.getElementById("pauseVideoBtn");
+
+playVideoBtn.addEventListener("click", () => {
+  video2.play();
+  playVideoBtn.classList.add("iwh-hidden");
+  pauseVideoBtn.classList.remove("iwh-hidden");
+});
+
+pauseVideoBtn.addEventListener("click", () => {
+  video2.pause();
+  playVideoBtn.classList.remove("iwh-hidden");
+  pauseVideoBtn.classList.add("iwh-hidden");
+});
