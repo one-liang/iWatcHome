fae8077103d2f8f756bf75e187f393d8f7d178df
one-liang <william810212@gmail.com>
2024-07-12 11:53:31 +0800

    feat: add scroll event

--- src/index.js
+++ src/index.js
@@ -30,4 +30,33 @@ document.addEventListener("DOMContentLoaded", function () {
 
   playVideoBtn.addEventListener("click", toggleVideoPlayState);
   pauseVideoBtn.addEventListener("click", toggleVideoPlayState);
+
+  const scrollBtn1 = document.getElementById("scrollBtn1");
+  const scrollBtn2 = document.getElementById("scrollBtn2");
+  const scrollBtn3 = document.getElementById("scrollBtn3");
+  const scrollBtn4 = document.getElementById("scrollBtn4");
+
+  scrollBtn1.addEventListener("click", function () {
+    document.getElementById("section3").scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+
+  scrollBtn2.addEventListener("click", function () {
+    document.getElementById("section4").scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+
+  scrollBtn3.addEventListener("click", function () {
+    document.getElementById("section6").scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+
+  scrollBtn4.addEventListener("click", function () {
+    document.getElementById("section7").scrollIntoView({
+      behavior: "smooth",
+    });
+  });
 });
