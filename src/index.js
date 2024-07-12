df99dee8a6d8988de1196309fb62a0b765d36726
one-liang <william810212@gmail.com>
2024-07-12 11:39:27 +0800

    chore: revise video button display logic

--- src/index.js
+++ src/index.js
@@ -9,15 +9,25 @@ document.addEventListener("DOMContentLoaded", function () {
   const playVideoBtn = document.getElementById("playVideoBtn");
   const pauseVideoBtn = document.getElementById("pauseVideoBtn");
 
-  playVideoBtn.addEventListener("click", () => {
-    video2.play();
-    playVideoBtn.classList.add("iwh-hidden");
-    pauseVideoBtn.classList.remove("iwh-hidden");
-  });
+  function toggleVideoPlayState() {
+    if (video2.paused) {
+      video2.play();
+      playVideoBtn.classList.add("iwh-hidden");
+      pauseVideoBtn.classList.remove("iwh-hidden");
 
-  pauseVideoBtn.addEventListener("click", () => {
-    video2.pause();
-    playVideoBtn.classList.remove("iwh-hidden");
-    pauseVideoBtn.classList.add("iwh-hidden");
-  });
+      setTimeout(() => {
+        pauseVideoBtn.classList.add("!iwh-opacity-0");
+      }, 1000);
+    } else {
+      video2.pause();
+      playVideoBtn.classList.remove("iwh-hidden");
+      pauseVideoBtn.classList.add("iwh-hidden");
+      pauseVideoBtn.classList.remove("!iwh-opacity-0");
+    }
+  }
+
+  video2.addEventListener("click", toggleVideoPlayState);
+
+  playVideoBtn.addEventListener("click", toggleVideoPlayState);
+  pauseVideoBtn.addEventListener("click", toggleVideoPlayState);
 });
