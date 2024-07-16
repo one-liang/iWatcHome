2ab746ba7416ac4e38c8a5daf3d5ec85f29c7579
one-liang <111212776+for-demo@users.noreply.github.com>
2024-07-16 22:21:24 +0800

    chore: remove video2, update meta description

--- src/index.js
+++ src/index.js
@@ -7,32 +7,6 @@ document.addEventListener("DOMContentLoaded", function () {
     video1.play();
   }
 
-  const video2 = document.getElementById("video2");
-  const playVideoBtn = document.getElementById("playVideoBtn");
-  const pauseVideoBtn = document.getElementById("pauseVideoBtn");
-
-  function toggleVideoPlayState() {
-    if (video2.paused) {
-      video2.play();
-      playVideoBtn.classList.add("iwh-hidden");
-      pauseVideoBtn.classList.remove("iwh-hidden");
-
-      setTimeout(() => {
-        pauseVideoBtn.classList.add("!iwh-opacity-0");
-      }, 1000);
-    } else {
-      video2.pause();
-      playVideoBtn.classList.remove("iwh-hidden");
-      pauseVideoBtn.classList.add("iwh-hidden");
-      pauseVideoBtn.classList.remove("!iwh-opacity-0");
-    }
-  }
-
-  video2.addEventListener("click", toggleVideoPlayState);
-
-  playVideoBtn.addEventListener("click", toggleVideoPlayState);
-  pauseVideoBtn.addEventListener("click", toggleVideoPlayState);
-
   const scrollBtn1 = document.getElementById("scrollBtn1");
   const scrollBtn2 = document.getElementById("scrollBtn2");
   const scrollBtn3 = document.getElementById("scrollBtn3");
